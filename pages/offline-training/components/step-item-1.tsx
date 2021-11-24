5a7806153ce6e99c0fdc46099efb5d889d068a9e
zhuchentong <2037630@163.com>
2021-11-24 16:34:31 +0800

    添加StepContainer支持

--- pages/offline-training/components/step-item-1.tsx
+++ pages/offline-training/components/step-item-1.tsx
@@ -1,15 +1,18 @@
 import {
   forwardRef,
   ForwardRefRenderFunction,
+  useContext,
   useImperativeHandle,
   useState
 } from 'react'
+import StepContainer from '~/shared/components/step-container'
 
 const StepItemContent1: ForwardRefRenderFunction<{}> = (
   props,
   ref
 ) => {
   const [value, updateValue] = useState(0)
+  const context = useContext(StepContainer.Context)
 
   useImperativeHandle(ref, () => ({
     onSubmit: () => {
@@ -20,6 +23,9 @@ const StepItemContent1: ForwardRefRenderFunction<{}> = (
   return (
     <section onClick={() => updateValue(value + 1)}>
       content: {value}
+      <button onClick={() => console.log(context)}>
+        12312
+      </button>
     </section>
   )
 }
